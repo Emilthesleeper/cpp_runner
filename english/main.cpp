42b490ac000f18bd63ed2cc98fb8c0585fc791bc
Emilthesleeper <91831917+Emilthesleeper@users.noreply.github.com>
2022-01-18 13:05:03 +0100

    Create main.cpp

---
+++ english/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string var = "Thanks for downloading.";
+    cout << var << endl;
+    cout << "from main.cpp" << endl;
+    return 0;
+}
+
+// This is a simple comment
+
+/*
+This is a longer comment
+*/
