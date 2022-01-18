4c5ee834f30d36c676739670128a27b943593b6c
Emilthesleeper <91831917+Emilthesleeper@users.noreply.github.com>
2022-01-18 13:47:40 +0100

    Create main.cpp

---
+++ deutsch/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string var = "Danke fürs herunterladen.";
+    cout << var << endl;
+    cout << "von main.cpp" << endl;
+    return 0;
+}
+
+// This is a simple comment
+
+/*
+This is a longer comment
+*/
