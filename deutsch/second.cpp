518ccfeb0a3b3f8b28b0b05a0c8e873b97d1c483
Emilthesleeper <91831917+Emilthesleeper@users.noreply.github.com>
2022-01-18 13:47:58 +0100

    Create second.cpp

---
+++ deutsch/second.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string var = "Danke fürs herunterladen.";
+    cout << var << endl;
+    cout << "von second.cpp" << endl;
+    return 0;
+}
