d382d5b2978057ea9d860264047216597c75d865
Emilthesleeper <91831917+Emilthesleeper@users.noreply.github.com>
2022-01-18 13:02:58 +0100

    Add files via upload

---
+++ cpp-runner.py
@@ -0,0 +1,68 @@
+from operator import sub
+import subprocess,os,glob
+import platform
+from tkinter import E
+from discord.ext.commands.errors import MissingPermissions
+platforms=platform.system()
+
+class MissingFile(Exception):
+    pass
+
+class IncorrectIntArgument(Exception):
+    pass
+
+rawpath=__file__.replace(os.getcwd(),"").split("/")[1:-1]
+between="/"
+str=""
+for a in rawpath:
+    str=str+a+between
+path2= str
+str=""
+for a in rawpath:
+    b=a.split(" ")
+    c=""
+    for d in b:
+        c=c+d
+    b=c
+    if not b==a:
+        a=f"\"{a}\""
+    str=str+a+between
+path=str
+
+subprocess.call("clear",shell=True)
+
+files=os.listdir(path2)
+
+for a in files:
+    if not a.endswith(".cpp"):
+        files.pop(files.index(a))
+
+if len(files)==0:
+    raise MissingFile("In this folder is no .cpp file.")
+
+if len(files)>=2:
+    str=""
+    for a in files:
+        str=f"{str}{files.index(a)+1}: {a}\n"
+    str=str+"\n"+": "
+    choice=input(str)
+    try:
+        files=files[int(choice)-1]
+    except Exception as e:
+        subprocess.call("clear",shell=True)
+        raise IncorrectIntArgument("This input is incorrect.")
+
+subprocess.call("clear",shell=True)
+
+while True:
+    subprocess.call(f"g++ -o r {path}{files}",shell=True)
+
+    if "win" in platforms.lower():
+        subprocess.call("r.exe",shell=True)
+    else:
+        subprocess.call("./r",shell=True)
+
+    x=input("Press Enter to go on. ")
+    subprocess.call("clear",shell=True)
+    if x !="":
+        exit()
---
+++ second.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string var = "Thanks for downloading.";
+    cout << var << endl;
+    cout << "from second.cpp" << endl;
+    return 0;
+}
